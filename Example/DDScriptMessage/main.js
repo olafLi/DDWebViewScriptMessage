a3779470562fa3bc3a7917dd7744e612fdfaf804
李腾飞 <litengfei@winkind-tech.com>
2018-04-04 18:07:59 +0800

    update

--- Example/DDScriptMessage/main.js
+++ Example/DDScriptMessage/main.js
@@ -2,7 +2,9 @@ function js_to_origin() {
     console.log(123);
     document.body.style.backgroundColor = document.body.style.backgroundColor == "red" ? "yellow" : "red";
 
-    window.cci.alert({'title':'123'},function(res){
-                     console.log(res);
+    window.cci.alert({
+        'title': '123'
+    }, function (res) {
+        console.log(res);
     })
 }
